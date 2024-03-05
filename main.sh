702b7849445fdd470ecd0f93e0e29b857c0439d3
Subhajit Sahu <wolfram77@gmail.com>
2024-03-05 12:15:32 +0530

    :bug: setup expt

--- main.sh
+++ main.sh
@@ -9,16 +9,23 @@ if [[ "$DOWNLOAD" != "0" ]]; then
   rm -rf $src
   git clone https://github.com/puzzlef/$src
   cd $src
+  git checkout input-large
 fi
 
 # Fixed config
 : "${TYPE:=double}"
 : "${MAX_THREADS:=64}"
-: "${REPEAT_BATCH:=5}"
+: "${REPEAT_BATCH:=1}"
 : "${REPEAT_METHOD:=1}"
 # Parameter sweep for batch (randomly generated)
 : "${BATCH_UNIT:=%}"
-: "${BATCH_LENGTH:=100}"
+: "${BATCH_LENGTH:=1}"
+: "${BATCH_DELETIONS_BEGIN:=0.00000002}"
+: "${BATCH_DELETIONS_END:=0.02}"
+: "${BATCH_DELETIONS_STEP:=*=10}"
+: "${BATCH_INSERTIONS_BEGIN:=0.00000008}"
+: "${BATCH_INSERTIONS_END:=0.08}"
+: "${BATCH_INSERTIONS_STEP:=*=10}"
 # Parameter sweep for number of threads
 : "${NUM_THREADS_MODE:=all}"
 : "${NUM_THREADS_BEGIN:=64}"
@@ -32,6 +39,12 @@ DEFINES=(""
 "-DREPEAT_METHOD=$REPEAT_METHOD"
 "-DBATCH_UNIT=\"$BATCH_UNIT\""
 "-DBATCH_LENGTH=$BATCH_LENGTH"
+"-DBATCH_DELETIONS_BEGIN=$BATCH_DELETIONS_BEGIN"
+"-DBATCH_DELETIONS_END=$BATCH_DELETIONS_END"
+"-DBATCH_DELETIONS_STEP=$BATCH_DELETIONS_STEP"
+"-DBATCH_INSERTIONS_BEGIN=$BATCH_INSERTIONS_BEGIN"
+"-DBATCH_INSERTIONS_END=$BATCH_INSERTIONS_END"
+"-DBATCH_INSERTIONS_STEP=$BATCH_INSERTIONS_STEP"
 "-DNUM_THREADS_MODE=\"$NUM_THREADS_MODE\""
 "-DNUM_THREADS_BEGIN=$NUM_THREADS_BEGIN"
 "-DNUM_THREADS_END=$NUM_THREADS_END"
@@ -43,17 +56,25 @@ nvcc ${DEFINES[*]} -std=c++17 -O3 -Xcompiler -fopenmp main.cu
 
 # Run on each temporal graph, with specified batch fraction
 runEach() {
-stdbuf --output=L ./a.out ~/Data/sx-mathoverflow.txt    248180   506550   239978   "$1" 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/sx-askubuntu.txt       1593160  964437   596933   "$1" 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/sx-superuser.txt       1940850  1443339  924886   "$1" 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/wiki-talk-temporal.txt 11401490 7833140  3309592  "$1" 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/sx-stackoverflow.txt   26019770 63497050 36233450 "$1" 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/indochina-2004.mtx  2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/uk-2002.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/arabic-2005.mtx     2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/uk-2005.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/webbase-2001.mtx    2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/it-2004.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/sk-2005.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/com-LiveJournal.mtx 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/com-Orkut.mtx       2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/asia_osm.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/europe_osm.mtx      2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/kmer_A2a.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/kmer_V1r.mtx        2>&1 | tee -a "$out"
 }
 
-# Run with different batch fractions
-runEach "0.00001"
-runEach "0.0001"
-runEach "0.001"
+# Run 5 times
+for i in {1..5}; do
+  runEach
+done
 
 # Signal completion
 curl -X POST "https://maker.ifttt.com/trigger/puzzlef/with/key/${IFTTT_KEY}?value1=$src$1"
